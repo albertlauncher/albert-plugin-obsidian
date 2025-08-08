be77b9abb118b688fbee276b3215c6488a7d63e8
Manuel Schneider <manuelschneid3r@gmail.com>
2025-08-08 15:00:45 +0200

    Initial

---
+++ i18n/obsidian_de.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Create</source>
+        <translation>Erstellen</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Öffnen</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Durchsuchen</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation type="vanished">Notiz</translation>
+    </message>
+    <message>
+        <source>Create new note in vault &apos;%1&apos;</source>
+        <translation type="vanished">Neue Notiz in Vault &apos;%1&apos; erstellen</translation>
+    </message>
+    <message>
+        <source>Create new note in &apos;%1&apos;</source>
+        <translation>Neue Notiz in &apos;%1&apos; erstellen</translation>
+    </message>
+</context>
+</TS>
